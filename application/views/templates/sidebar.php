c8f23bc9842a745e706a80de8d85161ff6ed3a6a
Machfudin <90607773+machfudin37@users.noreply.github.com>
2021-10-24 11:07:57 +0700

    Pertemuan 9

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -38,11 +38,6 @@
             <li class="nav-item active">
                 <!-- Nav Item - Dashboard -->
                 <li class="nav-item">
-                    <a class="nav-link pb-0" href="<?= base_url('buku/kategori'); ?>">
-                        <i class="fa fa-fw fa-book"></i>
-                        <span>Kategori Buku</span></a>
-                </li>
-                <li class="nav-item">
                     <a class="nav-link pb-0" href="<?= base_url('buku'); ?>">
                         <i class="fa fa-fw fa-book"></i>
                         <span>Data Buku</span></a>
@@ -56,44 +51,6 @@
 
         <!-- Divider -->
         <hr class="sidebar-divider mt-3">
-
-        <!--heading -->
-        <div class="sidebar-heading">
-            Transaksi
-        </div>
-        <!-- Nav Item - Dashboard -->
-        <li class="nav-item active">
-            <!-- Nav Item - Dashboard -->
-            <li class="nav-item">
-                <a class="nav-link pb-0" href="<?= base_url('pinjam'); ?>">
-                    <i class="fa fa-fw fa-shopping-cart"></i><span>Data Peminjaman</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link pb-0" href="<?= base_url('pinjam/daftarBooking'); ?>"><i class="fa fa-fw fa-list"></i><span>Data Booking</span></a>
-            </li>
-        </li>
-
-        <!-- Divider -->
-        <hr class="sidebar-divider mt-3">
-
-        <!-- Heading -->
-        <div class="sidebar-heading">
-            Laporan
-        </div>
-        <!-- Navbar Item Dasboard -->
-        <li class="nav-item active">
-            <!-- Navbar Item Dasboard -->
-            <li class="nav-item">
-                <a class="nav-link pb-0" href="<?= base_url('laporan/laporan_buku'); ?>"><i class="fa fa-fw fa-address-book"></i><span>Laporan Data Buku</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link pb-0" href="<?= base_url('laporan/laporan_anggota'); ?>"><i class="fa fa-fw fa-address-book"></i><span>Laporan Data Anggota</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link pb-0" href="<?= base_url('laporan/laporan_pinjam'); ?>"><i class="fa fa-fw fa-address-book"></i><span>Laporan Pinjam</span></a>
-            </li>
-        </li>
 
     <!-- Sidebar Toggler (Sidebar) -->
     <div class="text-center d-none d-md-inline">
