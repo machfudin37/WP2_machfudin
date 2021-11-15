ad12feb729af02728834a240f8fbca61e76470cb
machfudin37 <machfudin37@gmail.com>
2021-11-15 19:51:59 +0700

    Pertemuan 10

--- application/controllers/Autentifikasi.php
+++ application/controllers/Autentifikasi.php
@@ -131,5 +131,13 @@ class Autentifikasi extends CI_Controller
     {
       $this->load->view('autentifikasi/gagal');
     }
+    public function logout()
+    {
+    $this->session->unset_userdata('email');
+    $this->session->unset_userdata('role_id');
+
+    $this->session->set_flashdata('pesan', '<div class="alert alert-success alert-message" role="alert">Anda telah logout!!</div>');
+    redirect('autentifikasi');
+    }
 
 }
